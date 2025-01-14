5721b08f91290fa5f3b803c2a68f21c4bafa0d22
shivanshhgit <shivanshbansal21@gmail.com>
2025-01-14 22:05:34 +0530

    Added Data Access Layer

--- src/main/java/com/shivansh/first/student/StudentService.java
+++ src/main/java/com/shivansh/first/student/StudentService.java
@@ -1,20 +1,21 @@
 package com.shivansh.first.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,
-                        "Shivansh",
-                        "shivanshbansal21@gmail.com",
-                        LocalDate.of(2003, Month.SEPTEMBER, 29),
-                        21));
+        return studentRepository.findAll();
     }
 }
