b733d5065fbc862c1f5ded70cb1b03ca30592e38
shivanshhgit <shivanshbansal21@gmail.com>
2025-01-16 22:09:58 +0530

    PUT API added

--- src/main/java/com/shivansh/first/student/StudentService.java
+++ src/main/java/com/shivansh/first/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.shivansh.first.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 @Service
 public class StudentService {
 
@@ -34,5 +36,25 @@ public class StudentService {
             throw new IllegalStateException("Student with ID " + studentId + " does not exist!");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with ID " + studentId + " does not exist!"));
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email Taken!");
+            }
+            student.setEmail(email);
+        }
     }
 }
